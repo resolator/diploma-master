866772f9a38cac289afa94f49cb4584820a360e5
Vladyslav <v.dusiak@gmail.com>
2021-05-28 22:17:33 +0300

    Repo structure changed, updated IAMDataset class

--- scripts/train/iam_dataset.py
+++ scripts/train/iam_dataset.py
@@ -2,13 +2,21 @@
 # -*- coding: utf-8 -*
 """PyTorch wrapper for IAM dataset."""
 import cv2
+import torch
+
+import numpy as np
 
 from pathlib import Path
 from torch.utils.data import Dataset
 
 
 class IAMDataset(Dataset):
-    def __init__(self, images_dir, markup_filepath, split_filepath):
+    def __init__(self,
+                 images_dir,
+                 markup_filepath,
+                 split_filepath,
+                 height=119):
+        self.height = height
         self.markup = self._read_markup_file(markup_filepath)
 
         # read images from split only
@@ -21,9 +29,25 @@ class IAMDataset(Dataset):
     def __len__(self):
         return len(self.imgs_paths)
 
+    @staticmethod
+    def _resize(img, height):
+        """Resize image with saving aspect ratio."""
+        if img.shape[0] == height:
+            return img
+
+        # calculate ratio
+        ratio = height / img.shape[0]
+
+        return cv2.resize(img, None, fx=ratio, fy=ratio,
+                          interpolation=cv2.INTER_LINEAR)
+
     def __getitem__(self, idx):
         img_path = self.imgs_paths[idx]
-        img = cv2.imread(str(img_path))
+        img = cv2.imread(str(img_path), cv2.IMREAD_GRAYSCALE)
+        img = self._resize(img, self.height)
+
+        img = (torch.tensor(img).unsqueeze(0) / 255.0 - 0.5) * 2
+
         text = self.markup[img_path.stem]
 
         return img, text
@@ -43,7 +67,7 @@ class IAMDataset(Dataset):
             # make a record
             line = line.split(' ')
             name = line[0]
-            text = line[-1].split('|')
+            text = line[-1].split('|')[:-1]
             markup[name] = text
 
         return markup
@@ -56,6 +80,9 @@ class IAMDataset(Dataset):
         cv2.namedWindow('img', cv2.WINDOW_NORMAL)
         for i in range(n_samples):
             img, text = self[i]
+            img = ((img.squeeze() / 2 + 0.5) * 255).numpy().astype(np.uint8)
             print(' '.join(text))
             cv2.imshow('img', img)
             cv2.waitKey()
+
+        cv2.destroyWindow('img')
