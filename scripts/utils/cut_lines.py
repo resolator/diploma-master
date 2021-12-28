e8c75bed0f38500499a9af503a3710e5cdae6db5
Vladyslav <v.dusiak@gmail.com>
2021-12-28 18:44:43 +0300

    Developed a script to cut lines from IAM dataset

---
+++ scripts/utils/cut_lines.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*
+"""Cut lines from IAM dataset."""
+import cv2
+import argparse
+
+import numpy as np
+
+from tqdm import tqdm
+from pathlib import Path
+from xml.etree import ElementTree as et
+
+
+def get_args():
+    """Arguments parser."""
+    parser = argparse.ArgumentParser(description=__doc__)
+    
+    parser.add_argument('--img-dir', type=Path, required=True,
+                        help='Path to the IAM directory with images.')
+    parser.add_argument('--xml-dir', type=Path, required=True,
+                        help='Path to the directory with XML markup files.')
+    parser.add_argument('--save-to', type=Path, required=True,
+                        help='Path to save dir.')
+    parser.add_argument('--border-expand', type=int, default=35,
+                        help='Border expansion.')
+    
+    return parser.parse_args()
+
+
+def main():
+    """Application entry point."""
+    args = get_args()
+    
+    xmls = list(args.xml_dir.glob('*.xml'))
+    for xml_path in tqdm(xmls):
+        # read image
+        img_name = xml_path.stem + '.png'
+        img_path = str(args.img_dir.joinpath(img_name))
+        img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
+        
+        # read xml
+        xml_root = et.parse(xml_path).getroot()
+        for xml_line in list(list(xml_root)[1]):
+            # search sentence start\end
+            coords = []
+            for word in list(xml_line):
+                for pt in list(word):
+                    coords.append(int(pt.attrib['x']))
+            
+            left = min(coords)
+            right = max(coords)
+            
+            # cut line
+            xml_attrib = xml_line.attrib
+            upper_border = int(xml_attrib['uby']) - args.border_expand
+            lower_border = int(xml_attrib['lby']) + args.border_expand
+            left_border = np.clip(left - args.border_expand,
+                                  0, img.shape[-1])
+            right_border = np.clip(right + args.border_expand,
+                                   args.border_expand, img.shape[-1])
+            
+            line = img[upper_border:lower_border, left_border:right_border]
+            
+            # saving
+            dir_name = xml_path.stem.split('-')[0]
+            save_dir = args.save_to.joinpath(dir_name).joinpath(xml_path.stem)
+            save_dir.mkdir(parents=True, exist_ok=True)
+            
+            cv2.imwrite(str(save_dir.joinpath(xml_attrib['id'] + '.png')), line)
+
+
+if __name__ == '__main__':
+    main()
