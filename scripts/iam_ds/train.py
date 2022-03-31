b435be5f707cebaec20748ae4679821a6f773d70
Vladyslav <v.dusiak@gmail.com>
2022-03-31 16:49:21 +0300

    Added weight decay

--- scripts/iam_ds/train.py
+++ scripts/iam_ds/train.py
@@ -197,7 +197,8 @@ def main():
 
     epoch_step = get_epoch_step_fn(args.model_type)
     model = create_model(c2i, i2c, args=args).to(device)
-    optim = torch.optim.Adam(params=model.parameters(), lr=args.lr)
+    optim = torch.optim.Adam(params=model.parameters(), lr=args.lr,
+                             weight_decay=1e-5)
 
     # datasets
     ds_args = {'images_dir': args.images_dir,
