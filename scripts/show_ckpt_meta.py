Fixed params calculation

scripts/show_ckpt_meta.py
@@ -6,7 +6,7 @@ import argparse
 from pathlib import Path
 from pprint import pprint
 
-from common.utils import calc_params
+from common.utils import print_model_params
 
 
 def get_args():
@@ -23,9 +23,9 @@ def main():
     args = get_args()
 
     ckpt = torch.load(args.ckpt_path, map_location='cpu')
+    ckpt['model'].pop('pe.pe', None)
 
-    print('=' * 79)
-    print('Params num: {:,}'.format(calc_params(ckpt['model'].values())))
+    print_model_params(ckpt['model'].values(), False)
     print('Epoch number:', ckpt['epoch'])
     print('Metrics for this model:')
     pprint(ckpt['metrics'])