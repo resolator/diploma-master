70c8bcb8217c5ec9342a3429f2e757c20d1e2c0c
Vladyslav <v.dusiak@gmail.com>
2022-06-04 05:42:47 +0300

    Added one gate

--- scripts/common/conv_net.py
+++ scripts/common/conv_net.py
@@ -109,6 +109,9 @@ class ConvNet6(BaseNet):
                 nn.Dropout(0.4),
                 DepthwiseSepConv2D(out_channels, out_channels * 2, (1, 9)),
                 Gate(dim=[-2, -1]),
+                nn.Dropout(0.4),
+                DepthwiseSepConv2D(out_channels, out_channels * 2, (1, 9)),
+                Gate(dim=[-2, -1]),
                 nn.Dropout(0.4)
             )
 
